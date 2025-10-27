2c6be1931365b17275009f3151999431e06c1dd6
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-10-27 13:26:44 +0300

    add product

---
+++ Kotolud/src/main/java/com/example/kotolud/model/Product.java
@@ -0,0 +1,36 @@
+package com.example.kotolud.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+    private String description;
+    private String urlProduct;
+
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+
+}
