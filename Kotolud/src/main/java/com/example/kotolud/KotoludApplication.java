e0a087ee05b4ae6dd7b583bbc1deb2f529430997
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-10-27 12:39:20 +0300

    add url base

---
+++ Kotolud/src/main/java/com/example/kotolud/KotoludApplication.java
@@ -0,0 +1,13 @@
+package com.example.kotolud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotoludApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotoludApplication.class, args);
+    }
+
+}
---
+++ Kotolud/src/test/java/com/example/kotolud/KotoludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kotolud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotoludApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
