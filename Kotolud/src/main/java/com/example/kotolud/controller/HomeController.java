0e4405947ce4362fbadda6440d19811e4ab14471
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-10-29 02:21:44 +0300

    home V1

--- Kotolud/src/main/java/com/example/kotolud/controller/HomeController.java
+++ Kotolud/src/main/java/com/example/kotolud/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.example.kotolud.controller;
 
+import com.example.kotolud.dto.category.CategoryResponseDTO;
 import com.example.kotolud.dto.product.ProductResponseDTO;
 import com.example.kotolud.serivce.ProductService;
+import com.example.kotolud.serivce.category.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +15,21 @@ import java.util.List;
 @Controller
 public class HomeController {
     private final ProductService productService;
+    private final CategoryService categoryService;
 
 
 
     @GetMapping
     public String HomeController(Model model) {
+
+
         List<ProductResponseDTO> getLastProduct = productService.getLimitProduct(5);
         model.addAttribute("carusel", getLastProduct);
+
+        List<CategoryResponseDTO> categoryResponseDTOList = categoryService.findAll();
+        model.addAttribute("categoryResponseDTOList", categoryResponseDTOList);
+
+
         return "index";
     }
 
